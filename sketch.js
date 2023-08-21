7360f1284a88f49336c8d3c12806cd32099b081d
danielfalqui <100230158+danielfalqui@users.noreply.github.com>
2023-08-21 19:57:41 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+//porque ele não esta rodando?
+
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles,blastImage;
+var cars = [];
+
+function preload() {
+  backgroundImage = loadImage("./assets/planodefundo.png");
+  car1_img = loadImage("../assets/catapultC1.png");
+  car2_img = loadImage("../assets/catapultC1.png");
+  track = loadImage("../assets/PISTA.png");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  obstacle1Image = loadImage("./assets/Skeleton.png");
+  obstacle2Image = loadImage("./assets/Mimic_Chest.png");
+  lifeImage = loadImage("./assets/life.png");
+  blastImage = loadImage("./assets/blast.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+
+  if (gameState === 2) {
+    game.showLeaderboard();
+    game.end();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
